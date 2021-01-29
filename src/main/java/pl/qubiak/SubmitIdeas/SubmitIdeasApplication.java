b157a351910e951452482eed28efac3eb05ebc11
qubiak <kubiak1986@gmail.com>
2021-01-29 10:43:20 +0100

    init

---
+++ src/main/java/pl/qubiak/SubmitIdeas/SubmitIdeasApplication.java
@@ -0,0 +1,13 @@
+package pl.qubiak.SubmitIdeas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmitIdeasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubmitIdeasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/qubiak/SubmitIdeas/SubmitIdeasApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.qubiak.SubmitIdeas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmitIdeasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
